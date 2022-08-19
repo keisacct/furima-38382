27d43d6e0830b824f027e2fa08aaf8caa4fff97c
keisacct <107605705+keisacct@users.noreply.github.com>
2022-08-19 15:11:32 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-z0-9]+\z/ }
+  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kata, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kata, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :dob, presence: true
+
+end

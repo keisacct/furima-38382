990cc02499eeac34d176b8d2979a049ae85f21a7
keisacct <107605705+keisacct@users.noreply.github.com>
2022-08-23 15:16:25 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-    before_action :authenticate_user!, except: [:index]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :move_to_index, only: :edit
+
     
     def index
         @items = Item.all.order(created_at: "DESC")
@@ -18,10 +20,31 @@ class ItemsController < ApplicationController
         end
     end
 
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        if @item.save
+            redirect_to item_path
+        else
+            render :edit
+        end
+    end
+
     private
     def item_params
         params.require(:item).permit(:image, :title, :description, :price, 
             :category_id, :quality_id, :postage_id, :prefecture_id, 
             :shipping_day_id).merge(user_id: current_user.id)
+    end
+
+    def move_to_index
+        item = Item.find(params[:id])
+        unless user_signed_in? && item.user_id == current_user.id
+            redirect_to action: :index
+        end
     end
 end
